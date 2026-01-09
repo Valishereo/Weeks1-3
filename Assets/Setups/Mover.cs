207b6674063848e9e0cb719bf0e68e401a9c2069
orozcolv <orozcolv@OA-S141-10>
2026-01-09 17:49:45 -0500

    Mover and Chaser code works yay

---
+++ Assets/Setups/Mover.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+   public float speed = 0.02f;
+   public float xMax;
+   public float xMin;
+   public Camera gameCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moverXPos = transform.position;
+        moverXPos.x = moverXPos.x + speed;
+        transform.position = moverXPos;
+
+        Vector3 screenTransformPosition = gameCamera.WorldToScreenPoint(transform.position);
+        xMax = Screen.width;
+
+        xMin = 0;
+
+        if (xMax < screenTransformPosition.x)
+        {
+            speed *= -1;
+        }
+       
+        if (xMin > screenTransformPosition.x)
+        {
+            speed *= -1;
+        }
+
+    }
+}
