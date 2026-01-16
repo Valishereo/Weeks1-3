286be8f22b728ac3eb86b731d856ce77a5287914
orozcolv <orozcolv@OA-S141-11>
2026-01-16 17:16:31 -0500

    week 2's changes

---
+++ Assets/Setups/hexagon.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.LightTransport;
+
+public class hexagon : MonoBehaviour
+{
+
+    public AnimationCurve curve;
+    public float duration;
+
+    public float output;
+
+    private float progress = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progress += Time.deltaTime / duration;
+        output = curve.Evaluate(progress);
+
+        transform.localScale = Vector3.one * output;
+
+        if (progress > 1f)
+        {
+            progress = 0f;
+
+        }
+
+
+    }
+}
